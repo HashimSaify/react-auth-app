d7aadaad6fe6ee8da46651b6e9a8974cadcce2c2
Hashim Saify <786hashimsaify@gmail.com>
2025-12-08 21:33:07 +0530

    Update CORS to allow Vercel frontend

--- auth-backend/src/server.ts
+++ auth-backend/src/server.ts
@@ -15,9 +15,14 @@ const PORT = process.env.PORT || 5000;
 // ---------- GLOBAL MIDDLEWARES ---------- //
 
 // Allow requests from React frontend (http://localhost:3000)
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://reactauthapp-six.vercel.app', // ⬅️ replace with your actual Vercel URL
+];
+
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: allowedOrigins,
     credentials: true,
   })
 );
